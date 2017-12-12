c400deef82ae2f224ee3bc23e92afa1ae2be02f5
Deven Damiano <damiano.da96@gmail.com>
2017-12-12 13:53:35 -0500

    added prototypes

---
+++ sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void bubble_sort(int*);
+void selection_sort(int*);
+void insertion_sort(int*);
+void merge_sort(int*);
+void shell_sort(int*);
+void quick_sort(int*);
+
+int main()
+{
+    int* array = {4, 8, 2, 3, 6, 10, 1};
+    return 0;
+}
